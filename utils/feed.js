7343a0b5fc2283b1f26b829c7351601136835a04
perseveringman <15122337355@163.com>
2017-04-13 21:02:34 +0800

    微信小程序项目结构 包括基本框架和首页代码

---
+++ utils/feed.js
@@ -0,0 +1,23 @@
+var i_feed_1 = {
+      avatar: '/images/logo.jpg',
+      username: '陈静韬真的会修电脑喵',
+      sent_date: '1小时前',
+      feed_content: '南开大学与世界一流大学真的只有一堵墙南开大学与世界一流大学真的只有一堵墙南开大学与世界一流大学真的只有一堵墙南开大学与世界一流大学真的只有一堵墙南开大学与世界一流大学真的只有一堵墙南开大学与世界一流大学真的只有一堵墙南开大学与世界一流大学真的只有一堵墙南开大学与世界一流大学真的只有一堵墙南开大学与世界一流大学真的只有一堵墙南开大学与世界一流大学真的只有一堵墙南开大学与世界一流大学真的只有一堵墙南开大学与世界一流大学真的只有一堵墙南开大学与世界一流大学真的只有一堵墙南开大学与世界一流大学真的只有一堵墙南开大学与世界一流大学真的只有一堵墙',
+      like: 12,
+      comment: 12,
+      img_arr: ['/images/icon_tabbar.jpg', '/images/icon_tabbar.jpg', '/images/icon_tabbar.jpg', '/images/icon_tabbar.jpg', '/images/icon_tabbar.jpg', '/images/icon_tabbar.jpg', '/images/icon_tabbar.jpg', '/images/icon_tabbar.jpg']
+    };
+
+var i_feed_2 = {
+      avatar: '/images/logo.jpg',
+      username: '陈静韬',
+      sent_date: '1小时前',
+      feed_content: '南开大学与世界一流大学真的只有一堵墙南开大学与世界一流大学真的只有一堵墙南开大学与世界一流大学真的只有一堵墙南开大学与世界一流大学真的只有一堵墙南开大学与世界一流大学真的只有一堵墙南开大学与世界一流大学真的只有一堵墙南开大学与世界一流大学真的只有一堵墙南开大学与世界一流大学真的只有一堵墙南开大学与世界一流大学真的只有一堵墙南开大学与世界一流大学真的只有一堵墙南开大学与世界一流大学真的只有一堵墙南开大学与世界一流大学真的只有一堵墙南开大学与世界一流大学真的只有一堵墙南开大学与世界一流大学真的只有一堵墙南开大学与世界一流大学真的只有一堵墙',
+      like: 12,
+      comment: 12,
+      img_arr: ['/images/icon_tabbar.jpg', '/images/icon_tabbar.jpg', '/images/icon_tabbar.jpg', '/images/icon_tabbar.jpg', '/images/icon_tabbar.jpg', '/images/icon_tabbar.jpg', '/images/icon_tabbar.jpg', '/images/icon_tabbar.jpg']
+    };
+module.exports.i_feed_1 = i_feed_1;
+module.exports.i_feed_2 = i_feed_2;
+
+    
